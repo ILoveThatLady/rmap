0f6aea47de1a5c0d5c1a1f17c4fd9d2e63bbdf3f
Emilio Ortiz <ortizemilio.dev@gmail.com>
2023-06-17 13:14:33 -0600

    setting libraries

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-use std::net::TcpStream;
-
 fn main() {
-    let target_ip = "127.0.0.1";
-    let start_port = 1;
-    let end_point = 100;
-
-    for port in start_port..=end_point {
-        if is_port_open(&target_ip, port) {
-            println!("Port {} is open", port);
-        }
-    }
-}
-
-fn is_port_open(ip: &str, port: u16) -> bool {
-    if let Ok(stream) = TcpStream::connect((ip, port)) {
-        let _ = stream.shutdown(std::net::Shutdown::Both);
-        true
-    } else {
-        false
-    }
+    println!("Hello world!");
 }
